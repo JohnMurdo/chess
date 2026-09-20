84dfba97e417061b374440c29136507c9953ee7f
JohnMurdo <98349101+JohnMurdo@users.noreply.github.com>
2026-09-19 20:28:02 -0600

    Create piecemovescalc

---
+++ shared/src/main/java/chess/PieceMovesCalc.java
@@ -0,0 +1,10 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class PieceMovesCalc {
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        return new ArrayList<>();
+    }
+}
