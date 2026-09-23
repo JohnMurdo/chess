cf58b8a65f74179e2dd6037b0ae4423ac254fd71
JohnMurdo <98349101+JohnMurdo@users.noreply.github.com>
2026-09-22 19:53:52 -0600

    Pawn checks

--- shared/src/main/java/chess/MovePawn.java
+++ shared/src/main/java/chess/MovePawn.java
@@ -4,17 +4,23 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import static chess.ChessGame.TeamColor.WHITE;
+
 public class MovePawn extends PieceMovesCalc{
+    private
+
     public MovePawn(ChessBoard board) {
         super(board);
     }
 
     public Collection<ChessMove> pieceMoves(ChessPosition myPosition) {
-        List<int[]> offsets = new ArrayList<>();
-        Collection<ChessMove> moves = null;// findValidLines(myPosition, offsets);
-        offsets.add(new int[] {0,1});
+        ChessPiece startPiece = board.getPiece(myPosition);
+        int dir = startPiece.getTeamColor() == WHITE?1:-1;
+        int startRow = startPiece.getTeamColor() == WHITE? 2:7;
+        int endRow = startPiece.getTeamColor() == WHITE? 8:1;
 
-        return moves;
+
+        return null;
     }
     // check start (remember color matters)
     // check kills
